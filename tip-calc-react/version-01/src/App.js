f64b6514e0848b606a3defe4c13ae029bd44e1fd
John M. Wargo <johnwargo@users.noreply.github.com>
2020-12-28 15:42:07 -0500

    completed TipCalc Version 1

--- tip-calc-react/version-01/src/App.js
+++ tip-calc-react/version-01/src/App.js
@@ -1,56 +1,17 @@
 import './App.css';
 
-// https://flaviocopes.com/how-to-format-number-as-currency-javascript/
-const formatter = new Intl.NumberFormat('en-US', {
-  style: 'currency',
-  currency: 'USD',
-  minimumFractionDigits: 2
-})
 
 function App() {
-  return (
-    <div>
-      <h1 id="title">Tip Calculator (React)</h1>
-      <div className='costArea'>
-        <label for="mealCost">Meal Cost</label>
-        <input type="number" id="mealCost" min="0" value="" autoFocus />
-      </div>
-      <table cellSpacing="0">
-        <tr>
-          <th className='rating'>Rating</th>
-          <th className='tip'>Tip</th>
-          <th className='amount'>Amount</th>
-        </tr>
-        <tr>
-          <td className='rating'>Poor</td>
-          <td className='tip'>10%</td>
-          <td className='amount' id="tip10">$0.00</td>
-        </tr>
-        <tr>
-          <td className='rating'>OK</td>
-          <td className='tip'>15%</td>
-          <td className='amount' id="tip15">$0.00</td>
-        </tr>
-        <tr>
-          <td className='rating'>Good</td>
-          <td className='tip'>18%</td>
-          <td className='amount' id="tip18">$0.00</td>
-        </tr>
-        <tr>
-          <td className='rating'>Great</td>
-          <td className='tip'>20%</td>
-          <td className='amount' id="tip20">$0.00</td>
-        </tr>
-        <tr>
-          <td className='rating'>Excellent</td>
-          <td className='tip'>22%</td>
-          <td className='amount' id="tip22">$0.00</td>
-        </tr>
-      </table>
-    </div>
-  );
-  
-  function updateTipAmounts() {
+
+  const handleChange = event => {
+
+    // https://flaviocopes.com/how-to-format-number-as-currency-javascript/
+    const formatter = new Intl.NumberFormat('en-US', {
+      style: 'currency',
+      currency: 'USD',
+      minimumFractionDigits: 2
+    })
+
     // grab the meal cost from the page
     let mealCost = document.getElementById("mealCost").value;
     // Populate the table with tip amounts
@@ -60,6 +21,52 @@ function App() {
     document.getElementById('tip20').innerHTML = formatter.format(mealCost * 0.20);
     document.getElementById('tip22').innerHTML = formatter.format(mealCost * 0.22);
   }
+
+  return (
+    <div>
+      <h1 id="title">Tip Calculator (React)</h1>
+      <div className='costArea'>
+        <label for="mealCost">Meal Cost</label>
+        <input type="number" id="mealCost" min="0" defaultValue="0" autoFocus onChange={handleChange} />
+      </div>
+      <table cellSpacing="0">
+        <thead>
+          <tr>
+            <th className='rating'>Rating</th>
+            <th className='tip'>Tip</th>
+            <th className='amount'>Amount</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td className='rating'>Poor</td>
+            <td className='tip'>10%</td>
+            <td className='amount' id="tip10">$0.00</td>
+          </tr>
+          <tr>
+            <td className='rating'>OK</td>
+            <td className='tip'>15%</td>
+            <td className='amount' id="tip15">$0.00</td>
+          </tr>
+          <tr>
+            <td className='rating'>Good</td>
+            <td className='tip'>18%</td>
+            <td className='amount' id="tip18">$0.00</td>
+          </tr>
+          <tr>
+            <td className='rating'>Great</td>
+            <td className='tip'>20%</td>
+            <td className='amount' id="tip20">$0.00</td>
+          </tr>
+          <tr>
+            <td className='rating'>Excellent</td>
+            <td className='tip'>22%</td>
+            <td className='amount' id="tip22">$0.00</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  );
 
 }
 
