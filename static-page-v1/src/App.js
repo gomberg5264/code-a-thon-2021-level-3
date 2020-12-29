202f38e77948744d8b5af454b0a3f4fe807df2ec
John M. Wargo <johnwargo@users.noreply.github.com>
2020-12-29 13:50:08 -0500

    added static page

---
+++ static-page-v1/src/App.js
@@ -0,0 +1,26 @@
+import './App.css';
+
+function App() {
+  openPage(page) = {
+
+  }
+
+  return (
+    <div>
+      <header><h1>Static App (V1)</h1></header>
+      <p>Bacon ipsum dolor amet chuck pork chop drumstick pork, rump bresaola swine shankle landjaeger tri-tip filet mignon ham tenderloin. Ham hock strip steak cow jerky pig biltong, drumstick salami beef ribs pastrami fatback spare ribs flank tail. Venison cupim alcatra tongue, drumstick sirloin beef salami cow pork loin brisket jowl. Bresaola flank pork chop ham chislic. Shoulder pastrami sausage, frankfurter meatloaf corned beef pig chicken. Shank chislic spare ribs, turducken fatback swine short ribs ball tip shankle brisket meatball shoulder frankfurter kevin pork chop.</p>
+      <button onClick="openPAge(page1);">Learn More</button>
+      <p>Filet mignon beef chuck leberkas t-bone venison. Hamburger pork chop pork belly, brisket tail picanha leberkas. Frankfurter leberkas tenderloin venison ball tip tri-tip jowl picanha bacon alcatra pastrami pork andouille. Chicken jerky beef ribs ground round, buffalo kevin picanha tail capicola. Short ribs picanha prosciutto buffalo ribeye ham shankle frankfurter turducken tongue filet mignon doner leberkas. Bresaola alcatra buffalo kevin pork loin ham. Turducken andouille spare ribs swine venison.</p>
+      <button onClick="openPAge(page1);">Learn More</button>
+      <p>Tail strip steak jerky kevin, sausage shank salami. Chicken boudin cow pig bresaola corned beef. Landjaeger cupim bacon ball tip, pork chop kevin flank burgdoggen filet mignon alcatra picanha. Alcatra landjaeger pork chop strip steak meatloaf. Chicken pork loin biltong turducken. Pork kevin pork belly, sausage ribeye landjaeger biltong rump buffalo ball tip.</p>
+      <button onClick="openPAge(page1);">Learn More</button>
+      <p>Andouille alcatra leberkas, chuck tail pork doner turducken porchetta chislic. Pastrami tri-tip turkey chuck fatback tenderloin. Tail short loin meatloaf venison tongue ground round corned beef salami. Bresaola kevin porchetta beef ground round meatball turducken chicken t-bone.</p>
+      <button onClick="openPAge(page1);">Learn More</button>
+      <p>Frankfurter bacon chicken, turducken pork chop brisket meatloaf beef. Biltong chuck shoulder meatball, bacon tri-tip brisket meatloaf pork belly ball tip tongue ground round prosciutto pig beef ribs. Burgdoggen venison shankle, kevin bacon leberkas t-bone. Ball tip pastrami landjaeger ham spare ribs. Chuck strip steak pastrami ribeye. Beef burgdoggen venison, prosciutto filet mignon shank beef ribs corned beef cupim t-bone ham hock jowl pork brisket. Venison rump burgdoggen buffalo jerky kielbasa short ribs shoulder beef sausage.</p>
+      <button onClick="openPAge(page1);">Learn More</button>
+      <footer>Sample Static React App By John M. Wargo (why are you looking all the way down here?)</footer>
+    </div>    
+  );
+}
+
+export default App;
