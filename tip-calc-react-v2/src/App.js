5fdb5f7c95535ed3bf123d9a0196232db2726967
John M. Wargo <johnwargo@users.noreply.github.com>
2020-12-29 15:25:00 -0500

    working on tip calc V2

--- tip-calc-react-v2/src/App.js
+++ tip-calc-react-v2/src/App.js
@@ -1,34 +1,17 @@
+import React from 'react';
 import './App.css';
+
+const handleChange = event => {
+  // Update the meal cost property
+  
+}
 
 
 function App() {
-
-  const handleChange = event => {
-
-    // https://flaviocopes.com/how-to-format-number-as-currency-javascript/
-    const formatter = new Intl.NumberFormat('en-US', {
-      style: 'currency',
-      currency: 'USD',
-      minimumFractionDigits: 2
-    })
-
-    // grab the meal cost from the page
-    let mealCost = document.getElementById("mealCost").value;
-    // Populate the table with tip amounts
-    document.getElementById('tip10').innerHTML = formatter.format(mealCost * 0.10);
-    document.getElementById('tip15').innerHTML = formatter.format(mealCost * 0.15);
-    document.getElementById('tip18').innerHTML = formatter.format(mealCost * 0.18);
-    document.getElementById('tip20').innerHTML = formatter.format(mealCost * 0.20);
-    document.getElementById('tip22').innerHTML = formatter.format(mealCost * 0.22);
-  }
-
   return (
     <div>
       <h1 id="title">Tip Calculator (React)</h1>
-      <div className='costArea'>
-        <label for="mealCost">Meal Cost</label>
-        <input type="number" id="mealCost" min="0" defaultValue="0" autoFocus onChange={handleChange} />
-      </div>
+      <MealCostBlock />
       <table cellSpacing="0">
         <thead>
           <tr>
@@ -38,36 +21,38 @@ function App() {
           </tr>
         </thead>
         <tbody>
-          <tr>
-            <td className='rating'>Poor</td>
-            <td className='tip'>10%</td>
-            <td className='amount' id="tip10">$0.00</td>
-          </tr>
-          <tr>
-            <td className='rating'>OK</td>
-            <td className='tip'>15%</td>
-            <td className='amount' id="tip15">$0.00</td>
-          </tr>
-          <tr>
-            <td className='rating'>Good</td>
-            <td className='tip'>18%</td>
-            <td className='amount' id="tip18">$0.00</td>
-          </tr>
-          <tr>
-            <td className='rating'>Great</td>
-            <td className='tip'>20%</td>
-            <td className='amount' id="tip20">$0.00</td>
-          </tr>
-          <tr>
-            <td className='rating'>Excellent</td>
-            <td className='tip'>22%</td>
-            <td className='amount' id="tip22">$0.00</td>
-          </tr>
+          <TipRow rowLabel="Poor" rowPercent="10" />
+          <TipRow rowLabel="OK" rowPercent="15" />
+          <TipRow rowLabel="Good" rowPercent="18" />
+          <TipRow rowLabel="Great" rowPercent="20" />
+          <TipRow rowLabel="Excellent" rowPercent="22" />
         </tbody>
       </table>
     </div>
   );
+}
 
+class MealCostBlock extends React.Component {
+  render() {
+    return (
+      <div className='costArea'>
+        <label for="mealCost">Meal Cost</label>
+        <input type="number" id="mealCost" min="0" defaultValue="0" autoFocus onChange={handleChange} />
+      </div>
+    );
+  }
+}
+
+class TipRow extends React.Component {
+  render() {
+    return (
+      <tr>
+        <td className='rating'>{this.props.rowLabel}</td>
+        <td className='tip'>{this.props.rowPercent}%</td>
+        <td className='amount' id="tip10">$0.00</td>
+      </tr>
+    );
+  }
 }
 
 export default App;
